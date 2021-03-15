2eec929bd4bfefb3a8bc7296b7b3d40696c99ef0
Waleed Afzaal <62021811+trying3000@users.noreply.github.com>
2021-03-15 23:18:54 +0500

    Largest number and it's occurrence

--- Excercises.java
+++ Excercises.java
@@ -3,17 +3,25 @@ import java.util.*;
 public class Excercises {
 
 	public static void main(String[] args) {
+		int max = 0;
+		int count=0;
+		int num;
 		Scanner input = new Scanner(System.in);
 		
-		System.out.print("Enter an integer from 1-15: ");
-		int n = input.nextInt();
-		
-		for(int i = 1; i<=n ; i++) {
-			for(int j=1; j<=i; j++) {
-				System.out.print(j+ "  ");
+		System.out.print("Enter numbers: ");
+		do{
+			num = input.nextInt();
+			if(num>max) {
+				max = num;
+				count = 0;
 			}
-			System.out.println();
-		}
+			if (num==max) {
+				count++;
+			}
+		}while(num!=0);
+			System.out.println("Largest number is: "+ max);
+			System.out.print("Occurance: "+ count);
+		
 	}
 }
 		
